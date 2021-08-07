79e3bd9c3ba6d7673d26df32b163f7ad4fdc3417
Thiago Moura <thmoura14@hotmail.com>
2021-08-07 16:26:50 -0300

    Implemented test coverage

---
+++ tests/test_factory.py
@@ -0,0 +1,11 @@
+from flaskr import create_app
+
+
+def test_config():
+    assert not create_app().testing
+    assert create_app({'TESTING': True}).testing
+
+
+def test_health(client):
+    response = client.get('/health')
+    assert response.data == {'status': 'API is runnning...'}
